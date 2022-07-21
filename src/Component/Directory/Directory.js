4dd9b44f5b24e86f1b585776281282993c02ef3c
olamiji-8 <olamijisotomi@gmail.com>
2022-07-21 06:08:00 +0100

    Directory

---
+++ src/Component/Directory/Directory.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Directory.scss'
+
+const Directory = () => {
+  return (
+    <div>Directory</div>
+  )
+}
+
+export default Directory
---
+++ src/Component/Menu_item/Menu_item.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Menu_item = ({title}) => {
+  return (
+    <div>
+      <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'> HATS</h1>
+                        <span className='subtitle'>SHOP NOW </span>
+                    </div>
+
+                </div>
+
+            </div>
+    </div>
+  )
+}
+
+export default Menu_item
